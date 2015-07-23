08081febcf0ff27d980a4ce4ab6192cd492a96c3
unknown <cpersaud@DEV-CPERSAUD-US.cortera.local>
2015-07-23 15:57:29 -0400

    scanner loops thru records, 1000 each

--- scanner.rb
+++ scanner.rb
@@ -7,10 +7,26 @@
 	require g
 }
 
-url = 'https://data.cityofnewyork.us/resource/y6h5-jvss.json?$where=hearing_status=%27NEW%20ISSUANCE%27'
-sslVerifyHash = {
-	:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE
-}
+def getViolations(offsetNumber)
+	url = 'https://data.cityofnewyork.us/resource/y6h5-jvss.json?$where=hearing_status=%27NEW%20ISSUANCE%27&$offset='+offsetNumber.to_s
 
-responseJSON = open(url,sslVerifyHash).read
-responseArray = JSON.parse(responseJSON) # Length 1000
+	sslVerifyHash = {
+		:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE
+	}
+	responseJSON = open(url,sslVerifyHash).read
+	responseArray = JSON.parse(responseJSON) # Length 1000
+
+	if(responseArray.length===0)
+		return false
+	end
+
+	
+	responseArray.each{|violation|
+		p violation,
+		'======'
+	}
+
+	getViolations(offsetNumber+=1000)
+end
+
+getViolations(0)
