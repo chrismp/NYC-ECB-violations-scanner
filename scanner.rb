4d93fbac4565751d70c8c414a337f3f2a0281f75
unknown <>
2015-07-27 22:08:20 -0400

    script reads each each violation's info

--- scanner.rb
+++ scanner.rb
@@ -2,6 +2,7 @@
 	'open-uri',
 	'openssl',
 	'date',
+	'pp',
 	'json'
 ].each{|g|
 	require g
@@ -22,8 +23,32 @@ def getViolations(offsetNumber)
 
 	
 	responseArray.each{|violation|
-		p violation,
-		'======'
+		hearingLocation = violation['scheduled_hearing_location']
+		ticketNumber = violation['ticket_number']
+		respondentStreet = violation['respondent_address_street_name']
+		respondentZip = violation['respondent_address_zip_code']
+		respondentCity = violation['respondent_address_city']
+		respondentState = violation['respondent_address_state_name']
+		respondentBorough = violation['respondent_address_borough']
+		repondentFirstName = violation['respondent_first_name']
+		respondentLastName = violation['respondent_last_name']
+		violationStreetName = violation['violation_location_street_name']
+		violationBorough = violation['violation_location_borough']
+		violationState = violation['violation_location_state_name']
+		violationAmount = violation['total_violation_amount']
+		violationTime = violation['violation_time']
+		decisionDate = violation['decision_date']
+		complianceStatus = violation['compliance_status']
+		hearingResult = violation['hearing_result']
+		hearingStatus = violation['hearing_status']
+		hearingDate = violation['hearing_date']
+		hearingTime = violation['hearing_time']
+		balanceDue = violation['balance_due']
+		penalty = violation['penalty_imposed']
+		charge1Code = violation['charge_1_code']
+		charge1CodeSection = violation['charge_1_code_section']
+		charge1Description = violation['charge_1_code_description']
+		issuingAgency = violation['issuing_agency']
 	}
 
 	getViolations(offsetNumber+=1000)
